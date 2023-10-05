cb05ac945194d8a213d5eaad9f0987fd57a37042
maurovas <maurovas@DESKTOP-31DOKHH>
2023-10-04 22:16:08 -0500

    Data Context

---
+++ NorthWind.Repositories.EFCore/DataContext/NorthWindContext.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthWind.Entities.POCOEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Repositories.EFCore.DataContext
+{
+    public class NorthWindContext : DbContext
+    {
+        public NorthWindContext(
+            DbContextOptions<NorthWindContext> opcions) : 
+            base(opcions) { }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnModelCreating(
+        ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+                .Property(c => c.Id)
+                .HasMaxLength(5)
+                .IsFixedLength();
+            modelBuilder.Entity<Customer>()
+                .Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(40);
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(40);
+            modelBuilder.Entity<Order>()
+                .Property(o => o.CustomerId)
+                .IsRequired()
+                .HasMaxLength(5)
+                .IsFixedLength();
+            modelBuilder.Entity<Order>()
+                .Property(o => o.ShipAddress)
+                .IsRequired()
+                .HasMaxLength(60);
+            modelBuilder.Entity<Order>()
+                .Property(o => o.ShipCity)
+                .HasMaxLength(15);
+            modelBuilder.Entity<Order>()
+                .Property(o => o.ShipCountry)
+                .HasMaxLength(15);
+            modelBuilder.Entity<Order>()
+                .Property(o => o.ShipPostalCode)
+                .HasMaxLength(10);
+
+            modelBuilder.Entity<OrderDetail>()
+                .HasKey(od => new { od.OrderId, od.ProductId });
+
+            modelBuilder.Entity<Order>()
+            .HasOne<Customer>()
+            .WithMany()
+            .HasForeignKey(o => o.CustomerId);
+            modelBuilder.Entity<OrderDetail>()
+            .HasOne<Product>()
+            .WithMany()
+            .HasForeignKey(od => od.ProductId);
+
+            modelBuilder.Entity<Product>()
+            .HasData(
+             new Product { Id = 1, Name = "Chai"}, 
+             new Product { Id = 2, Name = "Chang"},
+             new Product { Id = 3, Name = "Anissed Syrup"}
+             );
+
+            modelBuilder.Entity<Customer>()
+            .HasData(
+             new Customer { Id = "ALFKI", Name = "Alfreds F."},
+             new Customer { Id = "ANATR", Name = "Ana Trujillo"},
+             new Customer { Id = "ANTON", Name = "Antonio Moreno"}
+             );
+
+        }
+
+    }
+}
---
+++ NorthWind.Repositories.EFCore/DataContext/NorthWindContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Repositories.EFCore.DataContext
+{
+    class NorthWindContextFactory : 
+        IDesignTimeDbContextFactory<NorthWindContext>
+    {
+        public NorthWindContext CreateDbContext(string[] args)
+        {
+            var OptionBuilder =
+                new DbContextOptionsBuilder<NorthWindContext>();
+            OptionBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;database=NorthWindDB");
+            return new NorthWindContext(OptionBuilder.Options);
+        }
+    }
+}
