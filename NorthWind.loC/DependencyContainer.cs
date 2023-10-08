c1cdc060dd97ad740f9b66bd8f24e4886fcb056f
maurovas <maurovas@DESKTOP-31DOKHH>
2023-10-08 12:40:21 -0500

    NorthWind.loC(DependencyContainer)

---
+++ NorthWind.loC/DependencyContainer.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NorthWind.Entities.Interfaces;
+using NorthWind.Repositories.EFCore.DataContext;
+using NorthWind.Repositories.EFCore.Repositories;
+using NorthWind.UseCases.Common.Behaviors;
+using NorthWind.UseCases.CreateOrder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.loC
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddNorthWindServices(
+         this IServiceCollection services,
+         IConfiguration configuration)
+        {
+            services.AddDbContext<NorthWindContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("NorthWindDB")));
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddMediatR(typeof(CreateOrderInteractor));
+            services.AddValidatorsFromAssembly(typeof(CreateOrderValidator).Assembly);
+            services.AddTransient(typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
+            return services;
+        }
+    }
+}
