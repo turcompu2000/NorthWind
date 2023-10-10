35770cae417f8db323b96f8367892a2990fabd9e
maurovas <maurovas@DESKTOP-31DOKHH>
2023-10-09 22:13:37 -0500

    Presenter(CreateOrder y Port)

---
+++ NorthWind.Presenters/IPresenter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Presenters
+{
+    public interface IPresenter<ResponseType, FormatType>: IOutputPort<ResponseType>
+    {
+        public FormatType Content { get; }
+    }
+}
