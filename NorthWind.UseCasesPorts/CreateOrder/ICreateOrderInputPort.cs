8605b5a3f842ab08666c0ce4d66bdb1e0f07f1b0
maurovas <maurovas@DESKTOP-31DOKHH>
2023-10-10 01:33:53 -0500

    UseCasesPort(InputPort y OutputPort)

---
+++ NorthWind.UseCasesPorts/CreateOrder/ICreateOrderInputPort.cs
@@ -0,0 +1,14 @@
+﻿using NorthWind.UseCasesDTOs.CreateOrder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.UseCasesPorts.CreateOrder
+{
+    public interface ICreateOrderInputPort
+    {
+        Task Handle(CreateOrderParams order);
+    }
+}
