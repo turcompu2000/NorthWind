a8a3fe63c10d0724bd88edb3b1ec00477cc4c570
maurovas <maurovas@DESKTOP-31DOKHH>
2023-10-10 11:58:50 -0500

    Validators

---
+++ NorthWind.UseCases/Common/Validators/Validator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.UseCases.Common.Validators
+{
+    public class Validator<Model>
+    {
+        public Task<List<ValidationFailure>> Validate(Model model,
+            IEnumerable<IValidator<Model>> validators, bool causesException = true)
+        {
+          var Failures = validators
+              .Select(v => v.Validate(model))
+              .SelectMany(r => r.Errors)
+              .Where(f => f != null)
+              .ToList();
+            if (Failures.Any() && causesException)
+            {
+                throw new ValidationException(Failures);
+            }
+            return Task.FromResult(Failures);
+        }
+    }
+}
