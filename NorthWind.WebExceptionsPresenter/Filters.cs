8d14119db1d33e27b4dfe1cbb317e00bd92d29ca
maurovas <maurovas@DESKTOP-31DOKHH>
2023-10-10 00:47:51 -0500

    NorthWind.WebApi

---
+++ NorthWind.WebExceptionsPresenter/Filters.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using NorthWind.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.WebExceptionsPresenter
+{
+    public static class Filters
+    {
+        public static void Register(MvcOptions opcions)
+        {
+          opcions.Filters.Add(new ApiExceptionFilterAttribute(
+            new Dictionary<Type,IExceptionHandler>
+            {
+            { typeof(GeneralException), new GeneralExceptionHandler()},
+            { typeof(ValidationException), new ValidationExceptionHandler()}
+            }
+            ));
+        }
+    }
+}
